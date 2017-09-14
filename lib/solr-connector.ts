bb84c23fd950c74cb1a9cb463604dc5aa32b2881
Andrew Wiens <adwiens@gmail.com>
2017-09-14 14:39:35 -0500

    Non-mutable CRUD works

--- lib/solr-connector.ts
+++ lib/solr-connector.ts
@@ -1,179 +1,16 @@
-import solr = require('solr-client');
-import uuid = require('uuid/v4');
+import * as solr from 'solr-client';
+import * as uuid from 'uuid/v4';
+import * as _ from 'lodash';
 
 var client: any;
 
 interface FindFilter {
-  fields?: any;     /* TODO */
-  include?: any;    /* TODO */
+  fields?: Object;     /* addFieldList and removeFields */
+  include?: any;    /* Not supported */
   limit?: number;   /* addRows */
-  order?: string;   /* TODO */
+  order?: string | Array<string>;   /* addSort */
   skip?: number;    /* addStart */
-  where?: any;      /* addQueryString */
-}
-
-class Query {
-  // Adds query string to the query Object based on filter.where
-  // See http://yonik.com/solr/query-syntax/
-  static addQueryString(filter: FindFilter, query: Object, id?: string): Object {
-    let queryString = '';
-    if (filter && filter.where) {
-      for (var prop in filter.where) {
-        queryString += '+' + prop + ':' + filter.where[prop] + ' ';
-      }
-      if (id) queryString += '+id:' + id;
-    }
-    else {
-      queryString = id ? ('+id:' + id) : '*:*';
-    }
-    query["q"] = queryString;
-    return query;
-  }
-
-  // Adds start field to the query Object based on filter.skip
-  static addStart(filter: FindFilter, query: Object): Object {
-    if (filter && filter.skip)
-      query["start"] = filter.skip + '';
-    return query;
-  }
-
-  // Adds rows field to the query Object based on filter.limit
-  static addRows(filter: FindFilter, query: Object): Object {
-    if (filter && filter.limit)
-      query["rows"] = filter.limit + '';
-    return query;
-  }
-
-  // Callback for find
-  static findCb(error: any, success: SolrGetSuccess, callback: Function) {
-    if (error) callback(error, undefined);
-    else callback(undefined, success.response.docs);
-  }
-  
-  // Callback for replaceOrCreate and replaceById
-  static replaceCb(error: any, success: Object, callback: Function, doc: Object) {
-    if (error) callback(error, undefined);
-    else {
-      client.commit(function(err,res) {
-        if(err) callback(err, undefined);
-        if(res) callback(undefined, doc);
-      });
-    }
-  }
-
-  // Callback for exists
-  static existsCb(error: any, success: SolrGetSuccess, callback: Function) {
-    if (error) callback(error, undefined);
-    if (success) callback(undefined, success.response.numFound > 0);
-  }
-}
-
-class Solr {
-  static connector: SolrConnector;
-
-  // get /hrms
-  // See https://loopback.io/doc/en/lb3/Querying-data.html
-  static find(filter: FindFilter, auth: Object, cb: Function) {
-    let query = {};
-    query = Query.addQueryString(filter, query);
-    query = Query.addStart(filter, query);
-    query = Query.addRows(filter, query);
-    client.get('query', query, (err, suc) => Query.findCb(err, suc, cb));
-  }
-
-  // put /hrms && post /hrms/replaceOrCreate
-  static replaceOrCreate(doc: Object, auth: Object, cb: Function) {
-    if (!doc["id"]) {
-      doc["id"] = uuid();
-      let query = {};
-      query = Query.addQueryString({}, query, doc["id"]);
-      client.get('query', query, (err, suc: SolrGetSuccess) => {
-        if (err) 
-          throw new Error("UUID collision check failed")
-        if (suc && suc.response.numFound === 0)
-          client.add([doc], (err, suc) => Query.replaceCb(err, suc, cb, doc));
-        else
-          throw new Error("UUID collision check failed")
-      });
-    }
-    else {
-      client.add([doc], (err, suc) => Query.replaceCb(err, suc, cb, doc));
-    }
-  }
-
-  // get /hrms/{id}
-  static findById(id: string, filter: FindFilter, auth: Object, cb: Function) {
-    let query = {};
-    query = Query.addQueryString(filter, query, id);
-    query = Query.addStart(filter, query);
-    query = Query.addRows(filter, query);
-    client.get('query', query, (err, suc) => Query.findCb(err, suc, cb));
-  }
-
-  // head /hrms/{id}
-  static exists(id: string, auth: Object, cb: Function) {
-    let query = {};
-    query = Query.addQueryString({}, query, id);
-    client.get('query', query, (err, suc) => Query.existsCb(err, suc, cb));
-  }
-
-  // put /hrms/{id} && post /hrms/{id}/replace
-  static replaceById(id: string, newDoc: Object, auth: Object, cb: Function) {
-    let query = {};
-    query = Query.addQueryString({}, query, id);
-    client.get('query', query, (err, suc: SolrGetSuccess) => {
-      if (err) cb(err, undefined);
-      if (suc) {
-        if (suc.response.numFound == 1) { /* id exists */
-          let origDoc = suc.response.docs[0];
-          for (var prop in origDoc) { /* merge fields */
-            if (prop[0] == '_') continue; /* skip Solr's built-in fields */
-            if (newDoc[prop] === undefined) newDoc[prop] = origDoc[prop];
-          }
-          client.add([newDoc], (err, suc) => Query.replaceCb(err, suc, cb, newDoc));
-        }
-        else if (suc.response.numFound > 1) cb({ /* id collision */
-          name: 'Found multiple documents with the same ID', 
-          status: '', 
-          message: 'More than one document shares this ID. This should not be possible.'
-        });
-        else cb({ /* id does not exist */
-          name: 'ID does not exist', 
-          status: '', 
-          message: 'Cannot replace by ID because a document with the given ID does not exist in the database.'
-        });
-      }
-    });
-  }
-
-  // delete /hrms/{id}
-  static deleteById(id: string, auth: Object, cb: Function) {
-    client.deleteByID(id, function(err, obj) {
-      if (err) cb({
-        name: 'Could not delete by ID',
-        status: '',
-        message: JSON.stringify(err)
-      });
-      else cb(undefined);
-    });
-    // TODO: commit
-  }
-
-  static count(p1: any, p2: any, p3: Function) {
-    console.log('Solr count');
-    console.log(p1); // where (get /hrms/count)
-    console.log(p2); // auth (get /hrms/count)
-    console.log(p3); // callback (get /hrms/count)
-    p3();
-  }
-
-  static findOne(p1: any, p2: any, p3: Function) {
-    console.log('Solr findOne');
-    console.log(p1); // filter (get /hrms/findOne)
-    console.log(p2); // auth (get /hrms/findOne)
-    console.log(p3); // callback (get /hrms/findOne)
-    p3();
-  }
+  where?: Object;   /* addQueryString; currently only equivalence is supported (see http://loopback.io/doc/en/lb2/Where-filter.html#where-clause-for-other-methods) */
 }
 
 interface SolrGetSuccess {
@@ -199,12 +36,159 @@ interface SolrDataSource {
   connector: SolrConnector;
 }
 
+class Query {
+  // Adds query string to the query Object based on filter.where
+  static addQueryString(filter: FindFilter, query: Object, id?: string): Object {
+    let queryString = '';
+    if (filter && filter.where) {
+      for (var prop in filter.where)
+        queryString += '+' + prop + ':' + filter.where[prop] + ' ';
+      if (id) queryString += '+id:' + id;
+    }
+    else queryString = id ? ('+id:' + id) : '*:*';
+    query["q"] = queryString;
+    return query;
+  }
+
+  // Adds sort field to the query Object based on filter.order
+  static addSort(filter: FindFilter, query: Object): Object {
+    if (filter && filter.order)
+      query["sort"] = (filter.order instanceof Array) ? filter.order.join() : filter.order;
+    return query;
+  }
+
+  // Returns whether all fields in the fields filter are set to false
+  static allFalse(filter: FindFilter): boolean {
+    for (var fieldName in filter.fields)
+      if (filter.fields[fieldName]) return false;
+    return true;
+  }
+
+  // Ensure filter.fields are always booleans
+  static fieldsToBoolean(filter: FindFilter): FindFilter {
+    for (var fieldName in filter.fields)
+      if (_.isString(filter.fields[fieldName]))
+        filter.fields[fieldName] = JSON.parse(filter.fields[fieldName].toLowerCase());
+    return filter;
+  }
+
+  // Adds field list to the query Object based on filter.fields
+  static addFieldList(filter: FindFilter, query: Object): Object {
+    if (filter && filter.fields && !Query.allFalse(filter)) {
+      let fl = [];
+      for (var fieldName in filter.fields)
+        if (filter.fields[fieldName]) fl.push(fieldName);
+      query["fl"] = fl.join();
+    }
+    return query;
+  }
+
+  // Adds start field to the query Object based on filter.skip
+  static addStart(filter: FindFilter, query: Object): Object {
+    if (filter && filter.skip) query["start"] = filter.skip + '';
+    return query;
+  }
+
+  // Adds rows field to the query Object based on filter.limit
+  static addRows(filter: FindFilter, query: Object): Object {
+    if (filter && filter.limit) query["rows"] = filter.limit + '';
+    return query;
+  }
+
+  // Call if allFalse() to remove fields from one document based on filter.fields
+  static removeFields(filter: FindFilter, doc: Object): Object {
+    if (filter && filter.fields)
+      for (var fieldName in filter.fields)
+        delete doc[fieldName];
+    return doc;
+  }
+
+  // Callback for find
+  static findCb(filter: FindFilter, error: any, success: SolrGetSuccess, callback: Function) {
+    if (error) callback(error, undefined);
+    else {
+      if (Query.allFalse(filter)) { // post-processing to remove unwanted fields if allFalse() -- see http://loopback.io/doc/en/lb2/Fields-filter.html
+        success.response.docs.forEach((ele, i, arr) => {
+          arr[i] = Query.removeFields(filter, ele);
+        });
+      }
+      callback(undefined, success.response.docs);
+    }
+  }
+
+  // Callback for exists
+  static existsCb(error: any, success: SolrGetSuccess, callback: Function) {
+    if (error) callback(error, undefined);
+    if (success) callback(undefined, success.response.numFound > 0);
+  }
+  
+  // Callback for findOne
+  static findOneCb(filter: FindFilter, error: any, success: SolrGetSuccess, callback: Function) {
+    if (error) callback(error, undefined);
+    else if (success.response.docs && success.response.docs.length > 0)
+      callback(undefined, Query.allFalse(filter) ? Query.removeFields(filter, success.response.docs[0]) : success.response.docs[0]); // post-processing to remove unwanted fields if allFalse() -- see http://loopback.io/doc/en/lb2/Fields-filter.html
+    else callback(undefined, []);
+  }
+}
+
+class Solr {
+  static connector: SolrConnector;
+
+  // Called by "get /hrms"
+  // See https://loopback.io/doc/en/lb3/Querying-data.html
+  static find(filter: FindFilter, auth: Object, cb: Function) {
+    filter = Query.fieldsToBoolean(filter);
+    let query = {};
+    query = Query.addQueryString(filter, query);
+    query = Query.addStart(filter, query);
+    query = Query.addRows(filter, query);
+    query = Query.addSort(filter, query);
+    query = Query.addFieldList(filter, query);
+    client.get('query', query, (err, suc) => Query.findCb(filter, err, suc, cb));
+  }
+
+  // Called by "get /hrms/{id}"
+  static findById(id: string, filter: FindFilter, auth: Object, cb: Function) {
+    filter = Query.fieldsToBoolean(filter);
+    let query = {};
+    query = Query.addQueryString(filter, query, id);
+    query = Query.addStart(filter, query);
+    query = Query.addRows(filter, query);
+    query = Query.addSort(filter, query);
+    query = Query.addFieldList(filter, query);
+    client.get('query', query, (err, suc) => Query.findCb(filter, err, suc, cb));
+  }
+
+  // Called by "head /hrms/{id}"
+  static exists(id: string, auth: Object, cb: Function) {
+    let query = {};
+    query = Query.addQueryString({}, query, id);
+    client.get('query', query, (err, suc) => Query.existsCb(err, suc, cb));
+  }
+
+  // Called by "get /hrms/findOne"
+  static findOne(filter: FindFilter, auth: Object, cb: Function) {
+    filter = Query.fieldsToBoolean(filter);
+    let query = {};
+    query = Query.addQueryString(filter, query);
+    query = Query.addStart(filter, query);
+    query = Query.addRows(filter, query);
+    query = Query.addSort(filter, query);
+    query = Query.addFieldList(filter, query);
+    client.get('query', query, (err, suc) => Query.findOneCb(filter, err, suc, cb));
+  }
+}
+
 export class SolrConnector {
   DataAccessObject: typeof Solr;
   dataSource: SolrDataSource;
 
   constructor(options: SolrConnectorOptions) {
-    client = solr.createClient(options.solr.host, options.solr.port, options.solr.core);
+    client = (solr as any).createClient( /* no TS type for solr-client */
+      options.solr.host, 
+      options.solr.port, 
+      options.solr.core
+    );
   }
 }
 
